71d02e265c5b0184b503b0face471d393b2bdbf5
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-04-15 03:13:34 -0500

    first commit

---
+++ models/Avatar.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const avatarSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    info: {
+      type: String,
+      default: 'Input details on the Avatar. ex: Avatar background story',
+    },
+    stats: {
+      health: { type: Number },
+      mana: { type: Number },
+      attack: { type: Number },
+      defence: { type: Number },
+      spclAttack: { type: Number },
+      spclDefence: { type: Number },
+      exp: { type: Number, default: 50 },
+    },
+    img: { type: String },
+
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+
+    personality: { type: String, default: '1,1,1,1,1,1' },
+  },
+
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Avatar', avatarSchema);
---
+++ server.js
@@ -0,0 +1,40 @@
+require('./config/database.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const app = express();
+const ejs = require('ejs');
+const indexRoute = require('./controllers/indexRoute.js');
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use((req, res, next) => {
+  next();
+});
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use('/', express.static(__dirname + '/public'));
+app.use(
+  session({
+    secret: 'clifford the big red dog',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 2,
+    },
+  })
+);
+
+// GET AT LOCAL HOST: /
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// ALL SEPRATE ROUTES
+app.use('/index', express.static(__dirname + '/public'));
+app.use('/index', indexRoute);
+app.listen(PORT, () => {
+  console.log('Server is running on port: ' + PORT);
+});
