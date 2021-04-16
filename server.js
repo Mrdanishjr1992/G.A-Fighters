f5c5336d474b1f16d3909558bb7b8d3c6efb7782
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-04-16 03:14:01 -0500

    nann

--- server.js
+++ server.js
@@ -10,31 +10,31 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use((req, res, next) => {
-  next();
+	next();
 });
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use('/', express.static(__dirname + '/public'));
 app.use(
-  session({
-    secret: 'clifford the big red dog',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7 * 2,
-    },
-  })
+	session({
+		secret: process.env.JWT_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 24 * 7 * 2,
+		},
+	})
 );
 
 // GET AT LOCAL HOST: /
 app.get('/', (req, res) => {
-  res.render('index');
+	res.render('index');
 });
 
 // ALL SEPRATE ROUTES
 app.use('/index', express.static(__dirname + '/public'));
 app.use('/index', indexRoute);
 app.listen(PORT, () => {
-  console.log('Server is running on port: ' + PORT);
+	console.log('Server is running on port: ' + PORT);
 });
