38d492b2b8b7c372bc90793d447ba619521ca148
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-04-16 00:37:39 -0500

    env variables

--- config/database.js
+++ config/database.js
@@ -1,25 +1,24 @@
 const mongoose = require('mongoose');
 const connectionString =
-  'mongodb+srv://mrdainshjr1992:8723050822@cluster0.o0aaq.mongodb.net/test?authSource=admin&replicaSet=atlas-13gmg9-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true' ||
-  'mongodb://localhost:27017/fighters';
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/fighters';
 
 mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 });
 mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected`);
+	console.log(`Mongoose connected`);
 });
 mongoose.connection.on('error', (err) => {
-  console.log(`Mongoose connected error :` + err);
+	console.log(`Mongoose connected error :` + err);
 });
 mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose disconnected');
+	console.log('Mongoose disconnected');
 });
 
 module.exports = {
-  User: require('../models/User'),
-  Avatar: require('../models/Avatar'),
+	User: require('../models/User'),
+	Avatar: require('../models/Avatar'),
 };
